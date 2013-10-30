7dd3c4a9e2e992a75069d41b2e196acf007c18c0
Riccardo Faveto <Riccardo@MacBook.local>
2013-10-30 09:43:56 +0100

    Initial Commit

---
+++ MyMovies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyMovies
+//
+//  Created by Riccardo Faveto on 30/10/13.
+//  Copyright (c) 2013 Riccardo Faveto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
